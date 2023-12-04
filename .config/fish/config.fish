ed0499f270b2e9aa504eb7c8a118fd3fbc982a6a
Pipelines Bot | WashU IT <blakea@wustl.edu>
2023-12-04 15:13:18 -0800

    Changes

--- .config/fish/config.fish
+++
@@ -1,6 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-	alias dotfiles='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-	dotfiles config --local status.showUntrackedFiles no
-	
-end
