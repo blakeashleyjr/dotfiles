7123cafa765288b2c3c76fd8ba95498383bf67f2
Blake Ashley <56891775+blakeashleyjr@users.noreply.github.com>
2023-12-07 12:48:28 -0800

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,7 @@
 # Check if the current session is interactive
 if status is-interactive
     # Configure Tide
-    tide configure --auto --style=Lean --prompt_colors='True color' --show_time='24-hour format' --lean_prompt_height='One line' --prompt_spacing=Compact --icons='Few icons' --transient=no
+    tide configure --auto --style=Lean --prompt_colors='True color' --show_time='24-hour format' --lean_prompt_height='One line' --prompt_spacing=Compact --icons='Few icons' --transient=No
 
     # Set up Fish PATH
     set -x PATH $PATH /usr/lib/ssh
